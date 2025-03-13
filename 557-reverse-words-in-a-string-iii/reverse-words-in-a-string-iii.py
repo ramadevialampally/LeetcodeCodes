c75a471a6f7d486d51d7bc06f5e2ceb8f8df0841
ALAMPALLY RAMADEVI <163161953+ramadevialampally@users.noreply.github.com>
2025-03-13 10:41:29 +0530

    Time: 1 ms (68.32%) | Memory: 18.8 MB (13.28%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s=s.split()
+        res=""
+        for i in range(len(s)):
+            
+            res+=s[i][::-1]
+            if i!=(len(s)-1):
+                res+=" "
+          
+        return res
+        
