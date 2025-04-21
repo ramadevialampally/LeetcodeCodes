d9331b47092f105f7bef0986b400c3d9894944e8
ALAMPALLY RAMADEVI <163161953+ramadevialampally@users.noreply.github.com>
2025-04-21 15:14:06 +0530

    Time: 945 ms (86.35%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select  a.name,COALESCE(b.bonus, Null) AS bonus  from
-Employee  a left join bonus b
-on a.empid=b.empid
+select n.name,b.bonus 
+from employee n left join bonus b
+on n.empid=b.empid
 where b.bonus<1000 or b.bonus is Null;
